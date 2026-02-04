379a7e4bb7f585806f312ee484bbf186cdeb8cc0
kenas134 <kirubel.tba@gmail.com>
2026-02-04 17:52:53 +0300

    Implement power calculation with modulo

---
+++ pow/pow.py
@@ -0,0 +1,8 @@
+from math import pow
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a = int(input())
+b = int(input())
+m = int(input())
+
+print(int(pow(a,b)))
+print(int(pow(a,b)%m))
