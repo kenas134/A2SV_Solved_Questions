6b3f3d00b98c55c7101396b8cfc4d5c75335238f
kenas134 <kirubel.tba@gmail.com>
2026-02-11 14:49:36 +0300

    Time: 4 ms (64.51%), Space: 19.4 MB (38.19%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,6 +1,6 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        roman = {'M': 1000, 'CM': 900, 'D': 500, 'CD': 400, 'C': 100, 'XC': 90, 'L': 50, 'XL': 40, 'X': 10, 'IX': 9, 'V': 5, 'IV': 4, 'I': 1}
+        roman = {"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
         
         ans = 0
         last = 0
