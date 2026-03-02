85a05aec017dbe5812fc282995354aff83edb5f9
kenas134 <kirubel.tba@gmail.com>
2026-03-02 14:06:21 +0300

    Time: 2 ms (46.58%), Space: 19.4 MB (63.61%) - LeetHub

--- 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.py
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.py
@@ -1,24 +1,29 @@
+from typing import List
+
 class Solution:
     def diffWaysToCompute(self, expression: str) -> List[int]:
         
-
-        def helper(expr):
+        def helper(left, right):
             res = []
 
-            for i in range(len(expr)):
-                if expr[i] in "+*-":
-                    left = helper(expr[:i])
-                    right = helper(expr[i+1:])
+            for i in range(left, right + 1):
+                if expression[i] in "+*-":
+                    left_a = helper(left, i - 1)
+                    right_a = helper(i + 1, right)
 
-                    for l in left:
-                        for r in right:
-                            if expr[i] == "+":
-                                res.append(l+r)
-                            elif expr[i] == "-":
-                                res.append((l-r))
+                    for l in left_a:
+                        for r in right_a:
+                            if expression[i] == "+":
+                                res.append(l + r)
+                            elif expression[i] == "-":
+                                res.append(l - r)
                             else:
-                                res.append(l*r)
+                                res.append(l * r)
+
+
             if not res:
-                return [int(expr)]
+                return [int(expression[left:right + 1])]
+
             return res
-        return helper(expression)
+
+        return helper(0, len(expression) - 1)
