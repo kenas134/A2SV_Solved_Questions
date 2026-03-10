c75ab2584da5b973959221bc48c7da20029814c6
kenas134 <kirubel.tba@gmail.com>
2026-03-10 18:50:20 +0300

    Time: 126 ms (66.69%), Space: 21.1 MB (27.8%) - LeetHub

---
+++ 0690-employee-importance/0690-employee-importance.py
@@ -0,0 +1,31 @@
+"""
+# Definition for Employee.
+class Employee:
+    def __init__(self, id: int, importance: int, subordinates: List[int]):
+        self.id = id
+        self.importance = importance
+        self.subordinates = subordinates
+"""
+
+class Solution:
+    def getImportance(self, employees: List['Employee'], id: int) -> int:
+        graph = {}
+        for node in employees:
+            i,im,neibour = node.id,node.importance,node.subordinates
+            graph[i] = [im,neibour]
+
+        visited = set()
+        def dfs(node):
+            if node in visited:
+                return 0
+            visited.add(node)
+            size = graph[node][0]
+            for neigbour in graph[node][1]:
+                size += dfs(neigbour)
+            return size
+        return dfs(id)
+
+        
+
+        
+
