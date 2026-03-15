247f02c99273cc5372e7af791a2b462095b51cdc
kenas134 <kirubel.tba@gmail.com>
2026-03-16 00:01:43 +0300

    Time: 11 ms (56.52%), Space: 19.7 MB (59.16%) - LeetHub

---
+++ 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.py
@@ -0,0 +1,32 @@
+class Solution:
+    def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
+        chk = {}
+        
+        
+        for i in range(len(list1)):
+            if list1[i] not in chk:
+                chk[list1[i]] = i
+        
+        
+        for i in range(len(list2)):
+            if list2[i] in chk:
+                chk[list2[i]] += i
+        
+        z = set(list2)
+       
+        chk = {k: v for k, v in chk.items() if k in z}
+        
+        
+        ans = list(chk.items())
+        ans.sort(key=lambda x: x[1])
+        
+        ans2 = set()
+        val = ans[0][1]
+        
+        for i in ans:
+            if i[1] == val:
+                ans2.add(i[0])
+            else:
+                break
+        
+        return list(ans2)
