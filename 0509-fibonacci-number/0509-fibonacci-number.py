86a05ca8ae88649f48b7f200c11da999db403ca0
kenas134 <kirubel.tba@gmail.com>
2026-03-16 18:18:06 +0300

    Time: 345 ms (5.03%), Space: 19.4 MB (15.75%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,9 +1,13 @@
 class Solution:
     def fib(self, n: int) -> int:
         def rec(n):
+            # n is the state
+            
+            # Base case
             if n == 0:
                 return 0
             if n == 1:
                 return 1
+            # recurence relation
             return rec(n-1) + rec(n-2)
         return rec(n)
