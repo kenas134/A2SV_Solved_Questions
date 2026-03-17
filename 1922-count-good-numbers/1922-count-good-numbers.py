696670201fb030fee5029832e7d08fc66f56d8df
kenas134 <kirubel.tba@gmail.com>
2026-03-17 18:36:24 +0300

    Time: 0 ms (100%), Space: 19.1 MB (97.68%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.py
+++ 1922-count-good-numbers/1922-count-good-numbers.py
@@ -4,4 +4,4 @@ class Solution:
         
         even = ceil(n / 2)
         odd = n//2
-        return (pow(5,even,pow(10,9) + 7)*pow(4,odd,pow(10,9) + 7))%(pow(10,9) + 7)
+        return pow(5,even,1000000007)*pow(4,odd,1000000007)%1000000007
