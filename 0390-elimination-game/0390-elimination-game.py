b3d66e2aa6a5d207808ed7834ff76789bf3caa42
kenas134 <kirubel.tba@gmail.com>
2026-03-19 19:22:53 +0300

    Time: 11 ms (6.77%), Space: 20.1 MB (8.99%) - LeetHub

--- 0390-elimination-game/0390-elimination-game.py
+++ 0390-elimination-game/0390-elimination-game.py
@@ -1,15 +1,15 @@
 class Solution:
     def lastRemaining(self, n: int) -> int:
-        def play(n, left):
+        def play(n, direction):
             if n == 1:
                 return 1
             
-            if left:  
-                return 2 * play(n // 2, False)
+            if direction == 0:  
+                return 2 * play(n // 2, 1)
             else:
                 if n % 2 == 1:
-                    return 2 * play(n // 2, True)
+                    return 2 * play(n // 2, 0)
                 else:
-                    return 2 * play(n // 2, True) - 1
+                    return 2 * play(n // 2, 0) - 1
         
-        return play(n, True)
+        return play(n, 0)
