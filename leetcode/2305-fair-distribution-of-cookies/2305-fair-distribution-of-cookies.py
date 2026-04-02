3940d29d5256a9975cb06ef807d16e67028121c3
kenas134 <kirubel.tba@gmail.com>
2026-04-03 02:05:28 +0300

    Add solution for Fair Distribution of Cookies

---
+++ leetcode/2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
@@ -0,0 +1,18 @@
+class Solution:
+    def distributeCookies(self, cookies: List[int], k: int) -> int:
+        
+        self.minn = float("inf")
+        kid = [0]*k
+        def dfs(idx):
+            if idx == len(cookies):
+                self.minn = min(self.minn,max(kid))
+                return
+            for i in range(k):
+                kid[i] += cookies[idx]
+                dfs(idx+1)
+                kid[i] -= cookies[idx]
+                if idx == 0 and kid[i] == 0:
+                    return
+
+        dfs(0)
+        return self.minn
