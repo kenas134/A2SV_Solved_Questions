dac850c8443dbfc195034346dad60ebb0cd869d7
kenas134 <kirubel.tba@gmail.com>
2026-04-03 01:40:15 +0300

    Add solution for Sudoku Solver

---
+++ leetcode/0037-sudoku-solver/0037-sudoku-solver.py
@@ -0,0 +1,43 @@
+class Solution:
+    def solveSudoku(self, board: List[List[str]]) -> None:
+        
+        rows = [set() for _ in range(9)]
+        cols = [set() for _ in range(9)]
+        boxes = [set() for _ in range(9)]
+        
+        # Initialize sets
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] != '.':
+                    num = board[i][j]
+                    rows[i].add(num)
+                    cols[j].add(num)
+                    boxes[(i // 3) * 3 + (j // 3)].add(num)
+        
+        def backtrack():
+            for i in range(9):
+                for j in range(9):
+                    if board[i][j] == '.':
+                        for num in '123456789':
+                            box_idx = (i // 3) * 3 + (j // 3)
+                            
+                            if num not in rows[i] and num not in cols[j] and num not in boxes[box_idx]:
+                                # place number
+                                board[i][j] = num
+                                rows[i].add(num)
+                                cols[j].add(num)
+                                boxes[box_idx].add(num)
+                                
+                                if backtrack():
+                                    return True
+                                
+                                # backtrack
+                                board[i][j] = '.'
+                                rows[i].remove(num)
+                                cols[j].remove(num)
+                                boxes[box_idx].remove(num)
+                        
+                        return False
+            return True
+        
+        backtrack()
