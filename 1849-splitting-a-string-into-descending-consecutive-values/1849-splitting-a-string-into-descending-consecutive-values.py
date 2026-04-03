555d7cd0d256daf4bb3e813e7c6c1014e948e61f
kenas134 <kirubel.tba@gmail.com>
2026-04-03 11:02:45 +0300

    Time: 5 ms (23.43%), Space: 19.4 MB (30.16%) - LeetHub

--- 1849-splitting-a-string-into-descending-consecutive-values/1849-splitting-a-string-into-descending-consecutive-values.py
+++ 1849-splitting-a-string-into-descending-consecutive-values/1849-splitting-a-string-into-descending-consecutive-values.py
@@ -1,34 +1,21 @@
 class Solution:
     def splitString(self, s: str) -> bool:
-        # splitted = []
-        # def back_track(idx):
-        #     if idx >= len(s) and len(splitted) >= 2:
-        #         return True
+        
+
+        ans = []
+
+        def dfs(idx):
+
+            if idx == len(s) and len(ans) >= 2:
+                return True
             
-        #     for i in range(idx, len(s)):
-        #         if not splitted or int(splitted[-1]) - int(s[idx:i+1]) == 1:
-        #             splitted.append(s[idx:i+1])
-        #             if back_track(i+1):
-        #                 return True
-        #             splitted.pop()
-        #     return False
-        # return back_track(0)
-
-        def dfs(idx,prev):
-
-            if idx == len(s):
-                return True
-            for j in range(idx,len(s)):
-                val = int(s[idx:j+1])
-                if val+1 > prev:
-                    return False
-                if val + 1 == prev and dfs(j+1,val):
-                    return True
-                    
+            for i in range(idx,len(s)):
+                if not ans or int(ans[-1])  == int(s[idx:i+1])+1:
+                    ans.append(s[idx:i+1])
+                    if dfs(i+1):
+                        return True
+                    ans.pop()
+            return False
+        return dfs(0)
 
 
-        for i in range(len(s)-1):
-            val = int(s[:i+1])
-            if dfs(i+1,val):
-                return True
-        return False
