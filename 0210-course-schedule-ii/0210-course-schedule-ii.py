ee2d4c4e4be94ffe892fad81436ad8d02f42937c
kenas134 <kirubel.tba@gmail.com>
2026-04-03 22:54:59 +0300

    Time: 3 ms (75.91%), Space: 21.1 MB (28.59%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -2,27 +2,31 @@ class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
 
         graph = defaultdict(list)
-        indeg = defaultdict(int)
         for c,p in prerequisites:
             graph[p].append(c)
-            indeg[c] += 1
-        
 
-        
-        queue = deque()
-        for i in range(numCourses):
-            if indeg[i] == 0:
-                queue.append(i)
+        def dfs(node,gray):
+            if node in gray:
+                return False
+
+            gray.add(node)
+            
+            for neb in graph[node]:
+                if neb not in visited:
+                    if not dfs(neb,gray):
+                        return False
+            ans.append(node)
+            gray.remove(node)
+            visited.add(node)
+            return True
+            
+
+        visited = set()
         ans = []
-        while queue:
-            cur = queue.popleft()
-            ans.append(cur)
-            for neb in graph[cur]:
-                indeg[neb] -= 1
-                if indeg[neb] == 0:
-                    queue.append(neb)
-        if len(ans) == numCourses:
-            return ans
-        return []
-
+        for i in range(numCourses):
+            if i not in visited:
+                if not dfs(i,set()):
+                    return []
+        return ans[::-1]
+        
         
