e68673b9e0c9e8e50ba2ce42249f2584eae1703b
kenas134 <kirubel.tba@gmail.com>
2026-04-14 15:35:05 +0300

    Time: 243 ms (61.28%), Space: 21.6 MB (77.16%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -2,26 +2,35 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         ROW = len(grid)
         COL = len(grid[0])
-        dir = [[1,0],[-1,0],[0,1],[0,-1]]
-        ans = 0
+
+        visited = [[0]*COL for i in range(ROW)]
+
+        di = [(1,0),(-1,0),(0,1),(0,-1)]
+        queue = deque()
         def bfs(i,j):
-            queue = deque()
-            grid[i][j] = "0"
             queue.append((i,j))
+            visited[i][j] = 1
             while queue:
-                cr,cc = queue.popleft()
-                
-                for dr,dc in dir:
-                    r = cr + dr
-                    c = cc + dc
-                    if   0 <= r < ROW and 0 <= c < COL and grid[r][c] == "1":
-                        queue.append((r,c))
-                        grid[r][c] = "0"
+                cx,cy = queue.popleft()
+
+                for dx,dy in di:
+                    x = cx + dx
+                    y = cy + dy
+
+                    if 0<= x < ROW and 0 <= y < COL and not visited[x][y] and grid[x][y] == "1":
+                        queue.append((x,y))
+                        visited[x][y] = 1
 
 
+
+
+        ans = 0
         for i in range(ROW):
             for j in range(COL):
-                if 0 <= i < ROW and 0 <= j < COL and grid[i][j] == "1":
-                    bfs(i,j)
+                if not visited[i][j] and grid[i][j] == "1":
                     ans += 1
+                    bfs(i,j)
+
         return ans
+
+        
